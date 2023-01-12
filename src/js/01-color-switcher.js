c634d2a4c6215a80e7a94ead4ae5ff54cef07f15
Dimon <bolilui.dmytro@gmail.com>
2023-01-12 16:55:39 +0200

    HW9 - TASK1 - in process

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+// get control elements
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStart.addEventListener('click', () => {
+  console.log('start');
+});
+
+btnStop.addEventListener('click', () => {
+  console.log('stop');
+});
