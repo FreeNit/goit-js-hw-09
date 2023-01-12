5f676205689b658baf5e2196dc37b97b0b7ff02a
FreeNit <bolilui.dmytro@gmail.com>
2023-01-12 14:56:21 +0000

    Deploying to gh-pages from @ FreeNit/goit-js-hw-09@c634d2a4c6215a80e7a94ead4ae5ff54cef07f15 🚀

---
+++ 01-color-switcher.898dea18.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{console.log("start")})),e.addEventListener("click",(()=>{console.log("stop")}));
+//# sourceMappingURL=01-color-switcher.898dea18.js.map
---
+++ 01-color-switcher.c40e3f83.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){console.log("start")})),o.addEventListener("click",(function(){console.log("stop")}))}();
+//# sourceMappingURL=01-color-switcher.c40e3f83.js.map
