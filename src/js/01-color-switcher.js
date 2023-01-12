b132d242e093791fcaa8d53ffe8599e177de3957
Dimon <bolilui.dmytro@gmail.com>
2023-01-12 21:06:47 +0200

    HW9 - Task1 - Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,27 @@
 // get control elements
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
-btnStart.addEventListener('click', () => {
-  console.log('start');
+let intervalID;
+
+btnStart.addEventListener('click', event => {
+  let generatedColor;
+
+  intervalID = setInterval(() => {
+    generatedColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = generatedColor;
+  }, 1000);
 });
 
 btnStop.addEventListener('click', () => {
-  console.log('stop');
+  if (!intervalID) {
+    return;
+  }
+  clearInterval(intervalID);
+  btnStart.removeAttribute('disabled');
 });
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
